13fa01607c288868c4bd68ac132f6beb8c89b4ea
Apporio Infolabs <apporioinfolabs@apporios-mbp.home>
2024-03-13 11:40:09 +0000

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DBH_Vitrine';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 }
