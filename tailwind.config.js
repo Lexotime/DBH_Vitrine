6f9f95f15217801492760c2118bc4e95fbab372a
Apporio Infolabs <apporioinfolabs@apporios-mbp.home>
2024-03-13 09:42:33 +0000

    finish init ...

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/pages/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
