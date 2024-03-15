2434c2b9e3ee8fe9b5aca63163565f2133d65d96
Apporio Infolabs <apporioinfolabs@apporios-mbp.home>
2024-03-15 13:34:50 +0000

    ...

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -8,8 +8,19 @@ import AOS from 'aos';
 })
 export class ServiceComponent implements OnInit {
 
+  public seeMenu = false;
+
   ngOnInit(): void {
     AOS.init();
+  }
+
+  toggleMenu(){
+    this.seeMenu = ! this.seeMenu;
+    if(this.seeMenu){
+      document.body.style.setProperty('overflow', 'hidden');
+    }else{
+      document.body.style.setProperty('overflow', 'scroll');
+    }
   }
 
 }
